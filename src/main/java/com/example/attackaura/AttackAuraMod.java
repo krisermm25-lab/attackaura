9db9bb8afa8d0f545c1989a9c08f1fe921f818c6
krisermm25-lab <krisermm25@gmail.com>
2026-06-11 05:09:48 +0300

    Update AttackAuraMod.java

--- src/main/java/com/example/attackaura/AttackAuraMod.java
+++ src/main/java/com/example/attackaura/AttackAuraMod.java
@@ -2,10 +2,13 @@ package com.example.attackaura;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("attackaura")
 public class AttackAuraMod {
     public AttackAuraMod() {
+        FMLJavaModLoadingContext.get().getModEventBus()
+            .addListener(AttackAuraHandler::registerKey);
         MinecraftForge.EVENT_BUS.register(new AttackAuraHandler());
     }
 }
